af85e48b9252ab89d446998bb44fb17866991344
mustafa laptop <ad16mkha@uwcad.it>
2024-08-22 14:23:36 -0400

    render images on auth only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,35 @@
-import Link from "next/link";
+import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
-const mockURLs = [
- "https://utfs.io/f/232ef12b-a07a-4a8f-8309-1b97e5df28b2-krr3f8.png",
- "https://utfs.io/f/af0c13d9-4489-42fc-b2a7-862d823bd904-pjuwh5.png",
- "https://utfs.io/f/e83a6280-9df3-46ec-9978-fa067de7beb6-pjxex1.png",
- "https://utfs.io/f/7a44e3c1-eecc-4423-a666-0230ec3dd5fb-pjuylr.png",
- "https://utfs.io/f/dfe42d58-c6fa-4bf4-b7ab-3f72f915efde-4j1ge8.png" 
-]
+async function Images(){
+  const images = await db.query.images.findMany();
+  return (
+    <div className="flex flex-wrap gap-4">
+      {
+        [...images, ...images, ...images].map(image => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+            <div>{image.name}</div>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
 
 export default async function HomePage() {
   // this function now only runs on the server -> console logs wont show in the browser
-  const images = await db.query.images.findMany();
+  
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {
-          [...images, ...images, ...images].map(image => (
-            <div key={image.id} className="w-48">
-              <img src={image.url} alt="image" />
-              <div>{image.name}</div>
-            </div>
-          ))
-        }
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl font-bold text-center">Please Sign In Above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
