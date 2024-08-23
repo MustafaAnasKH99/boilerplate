6c9f0c125e837683c0a698213dc76b3894888456
mustafa laptop <ad16mkha@uwcad.it>
2024-08-23 03:12:45 -0400

    upload images per user

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,17 +1,30 @@
+"use client"
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "../utils/uploadthing";
+import { useRouter } from "next/navigation";
 
 export function TopNav(){
+  const router = useRouter();
     return (
       <nav className="flex item-center justify-between w-full border-b p-4 text-xl font-semibold">
         <div>
           Top Nav
         </div>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        <div className="flex flex-row gap-20">
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UploadButton 
+              endpoint="imageUploader"
+              onClientUploadComplete={() => {
+                router.refresh();
+              }}
+            />
+            <UserButton />
+          </SignedIn>
+        </div>
       </nav>
     )
   }
